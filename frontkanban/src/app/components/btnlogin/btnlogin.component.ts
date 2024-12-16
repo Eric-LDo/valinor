f82b1ba6e97282ea6c7ed39e30c9ec81a683da97
Eric-LDo <121363351+Eric-LDo@users.noreply.github.com>
2024-12-15 23:50:02 -0300

    create the firsts components

---
+++ frontkanban/src/app/components/btnlogin/btnlogin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btnlogin',
+  imports: [],
+  templateUrl: './btnlogin.component.html',
+  styleUrl: './btnlogin.component.css'
+})
+export class BtnloginComponent {
+
+}
---
+++ frontkanban/src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LogofildComponent } from "../logofild/logofild.component";
+import { BtnloginComponent } from "../btnlogin/btnlogin.component";
+
+
+@Component({
+  selector: 'app-header',
+  imports: [LogofildComponent, BtnloginComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
